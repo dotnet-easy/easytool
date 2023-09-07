f0758b5ba3f7a486e3af1724031f00e431f482ff
TimChen <timchen@live.com>
2023-09-08 00:18:08 +0800

    增加clone的扩展函数

---
+++ EasyTool.Core/CloneCategory/CloneExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyTool.Extension
+{
+    public static class CloneExtension
+    {
+        //定义一个泛型方法，接受一个泛型参数 T，并返回一个 T 类型的对象
+        public static T Clone<T>(this T obj)=> CloneUtil.Clone(obj);
+    }
+}
---
+++ EasyTool.CoreTests/CloneCategory/CloneExtensionTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyTool.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyTool.Extension.Tests
+{
+    [TestClass()]
+    public class CloneExtensionTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            var obj1 = new First()
+            {
+                MyProperty1 = 1,
+                MyProperty2 = "A",
+                Second1 = new Second()
+                {
+                    MyProperty1 = 2,
+                    MyProperty2 = "B",
+                },
+                Second2 = new Second()
+                {
+                    MyProperty1 = 3,
+                    MyProperty2 = "C",
+                }
+            };
+            var obj2 = obj1.Clone();
+
+            Assert.AreEqual(obj1.MyProperty1, obj2.MyProperty1);
+            Assert.AreEqual(obj1.Second1.MyProperty1, obj2.Second1.MyProperty1);
+        }
+
+        [Serializable]
+        public class First
+        {
+            public int MyProperty1 { get; set; }
+            public string MyProperty2 { get; set; }
+
+            public Second Second1 { get; set; }
+
+            public Second Second2 { get; set; }
+        }
+
+        [Serializable]
+        public class Second
+        {
+            public int MyProperty1 { get; set; }
+            public string MyProperty2 { get; set; }
+        }
+    }
+}
