fcf4d50b3481ef286017061ac69009c3227186de
TimChen <timchen@live.com>
2023-09-10 11:20:33 +0800

    添加字符串判空扩展

---
+++ EasyTool.Core/ToolCategory/StrExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EasyTool.Extension
+{
+    public static class StrExtension
+    {
+        #region 文本可为空判断
+
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        #endregion
+    }
+}
