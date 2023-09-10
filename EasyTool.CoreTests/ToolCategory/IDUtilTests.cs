8fcc00af2e37cea10932df6efa62f1ce347b83dd
edwin <zj726381216@163.com>
2023-09-10 13:18:06 +0800

    update: 完善id生成工具

---
+++ EasyTool.CoreTests/ToolCategory/IDUtilTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyTool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EasyTool.Tests
+{
+    [TestClass]
+    public class IdUtilTests
+    {
+        [TestMethod]
+        public void NextSequenceUUID_AreGreaterThan()
+        {
+            var uuid1 = IdUtil.UUID(UUIDStyle.Sequence);
+            Thread.Sleep(10);
+            var uuid2 = IdUtil.UUID(UUIDStyle.Sequence);
+
+            Assert.IsTrue(uuid2.ToString().CompareTo(uuid1.ToString()) > 0);
+        }
+    }
+}
