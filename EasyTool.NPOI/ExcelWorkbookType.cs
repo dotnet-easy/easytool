f55b561c4c86f7ff74b0740f770f6c5810f28ed8
Hang <18258359477@163.com>
2023-09-30 00:31:10 +0800

    新增NPOI对工作簿对象工作表对象的相关操作

---
+++ EasyTool.NPOI/ExcelWorkbookType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyTool.NPOI;
+
+public enum ExcelWorkbookType
+{
+    /// <summary>
+    /// xls类型适用于Office2003及以下版本
+    /// </summary>
+    XLS,
+    /// <summary>
+    /// xlsx类型适用于Office2007及以上版本
+    /// </summary>
+    XLSX
+}
