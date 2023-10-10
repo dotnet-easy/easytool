1c8e5f9cdabb234b9bccef7f4e84283b82257dde
Sheldon-NULL <83170675+Sheldon-NULL@users.noreply.github.com>
2023-10-10 15:23:50 +0800

    Add AES加密添加上有IV的加密方式支持

--- EasyTool.Core/CodeCategory/AesUtil.cs
+++ EasyTool.Core/CodeCategory/AesUtil.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EasyTool.CodeCategory
 {
@@ -72,5 +75,98 @@ namespace EasyTool.CodeCategory
             }
             return false;
         }
+
+
+
+        /*
+         * AES Blowfish
+         * 对称加密算法的优点是速度快，
+         * 缺点是密钥管理不方便，要求共享密钥。
+         * 可逆对称加密  密钥长度 16 24 32 iv长度 16 
+         */
+
+        /// <summary>
+        /// AES 加密
+        /// </summary>
+        /// <param name="text">需要加密的值</param>
+        /// <param name="key">加密key</param>
+        /// <param name="iv">向量iv</param>
+        /// <param name="cipher">默认CSC</param>
+        /// <param name="padding">默认PKCS7</param>
+        /// <param name="encoding">默认UTF8</param>
+        /// <returns>加密后的结果</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string Encrypt(string text, string key, string iv ,CipherMode cipher = CipherMode.CBC, PaddingMode padding = PaddingMode.PKCS7, Encoding? encoding = null)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty; ;
+            if (!KeyIsLegalSize(key)) 
+                throw new ArgumentException("不合规的秘钥，请确认秘钥为16 、24、 32位的字符");
+            if (!IvIsLegalSize(iv)) 
+                throw new ArgumentException("不合规的iv，请确认iv为16位的字符");
+            encoding ??= Encoding.UTF8;
+
+            var plainTextBytes = encoding.GetBytes(text);
+            var keyBytes = encoding.GetBytes(key);
+            var ivBytes = encoding.GetBytes(iv);
+
+            using var symmetricKey = Aes.Create();
+            symmetricKey.Mode = cipher;
+            symmetricKey.Padding = padding;
+            using var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivBytes);
+            // 加密后的输出流
+            using var memoryStream = new MemoryStream();
+            // 将加密后的目标流（encryptStream）与加密转换（encryptTransform）相连接
+            using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+            // 将一个字节序列写入当前 CryptoStream （完成加密的过程）
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            var cipherTextBytes = memoryStream.ToArray();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+        /// <summary>
+        /// AES 解密
+        /// </summary>
+        /// <param name="text">需要解密的值</param>
+        /// <param name="key">解密key</param>
+        /// <param name="iv">向量iv</param>
+        /// <param name="cipher">默认CSC</param>
+        /// <param name="padding">默认PKCS7</param>
+        /// <param name="encoding">默认UTF8</param>
+        /// <returns>解密后的结果</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static string Decrypt(string encryptText, string key, string iv, CipherMode cipher = CipherMode.ECB, PaddingMode padding = PaddingMode.PKCS7, Encoding? encoding = null)
+        {
+            if (string.IsNullOrWhiteSpace(encryptText))
+                return string.Empty; ;
+            if (!KeyIsLegalSize(key))
+                throw new ArgumentException("不合规的秘钥，请确认秘钥为16 、24、 32位的字符");
+            if (!IvIsLegalSize(iv))
+                throw new ArgumentException("不合规的iv，请确认iv为16位的字符");
+            encoding ??= Encoding.UTF8;
+            var cipherTextBytes = Convert.FromBase64String(encryptText);
+            var keyBytes = encoding.GetBytes(key);
+            var ivBytes = encoding.GetBytes(iv);
+            using var symmetricKey = Aes.Create();
+            symmetricKey.Mode = cipher;
+            symmetricKey.Padding = padding;
+            // 用当前的 Key 属性和初始化向量 IV 创建对称解密器对象
+            using var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivBytes);
+            // 解密后的输出流
+            using var memoryStream = new MemoryStream(cipherTextBytes);
+            // 解密后的输出流
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            var plainTextBytes = new byte[cipherTextBytes.Length];
+            var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            return System.Text.Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+        }
+
+        private static bool KeyIsLegalSize(string sk)
+        {
+            int keyLength = sk.Length;
+            return keyLength == 16 || keyLength == 24 || keyLength == 32;
+        }
+        private static bool IvIsLegalSize(string iv) => iv.Length == 16;
     }
 }
